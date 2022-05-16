48a8762ecd84f2b28e2cddec581bbb6d7669deb0
Jorge <jandresgarcia513@gmail.com>
2022-05-16 06:06:06 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,9 @@
+﻿let links = Array.from(document.getElementsByClassName("quicklink"))
+
+links.forEach(setClickEvent)
+
+function setClickEvent(link) {
+    link.addEventListener("click", async () => {
+        chrome.tabs.create({url: link.getAttribute("href"), active: false});
+    });
+}
